e2674be3f36088aee894c9ba8a3a6118dd20407e
root <yravindraswamy@gamil.com>
2021-04-13 09:37:32 +0000

    exporting function with parameter

---
+++ exportFunctionWithParameter.js
@@ -0,0 +1,4 @@
+function getSquareOfNumber(number) {
+  return number * number;
+}
+module.exports = getSquareOfNumber;
---
+++ importFunctionWithParameter.js
@@ -0,0 +1,2 @@
+const squareOfNumber = require("./exportFunctionWithParameter.js");
+console.log(squareOfNumber(3));
